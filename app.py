cb228165203342c9a78b8dabaa0f58719984f59b
Uttam Pawar <uttam.pawar@amd.com>
2026-08-28 16:55:57 +0000

    moved init_db() outside main to call it as a module from gunicorn

--- app.py
+++ app.py
@@ -505,6 +505,7 @@ def _render_table(machines):
 
 # ── entry point ────────────────────────────────────────────────────────────────
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     app.run(host="0.0.0.0", port=5000, debug=False)
